504eb6f67f7e033f3d76f792be13c1dc9054600c
Widi Widayat <widayatwidi26@gmail.com>
2022-07-20 15:27:22 +0700

    add widget reusable and implement it

--- flutter_cubit_app/lib/pages/welcome_page.dart
+++ flutter_cubit_app/lib/pages/welcome_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
+import 'package:flutter_cubit/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -15,6 +19,19 @@ class _WelcomePageState extends State<WelcomePage> {
     "welcome-two.png",
     "welcome-three.png",
   ];
+
+  List textTitle = [
+    "Trip to Mountains",
+    "Adventure Months",
+    "Living in Nature"
+  ];
+
+  List subTitle = [
+    "Indonesia Chapter",
+    "Get your Pack soon!",
+    "Time to Heal"
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +50,31 @@ class _WelcomePageState extends State<WelcomePage> {
                 ),
                 fit: BoxFit.cover
               )
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: textTitle[index]),
+                      AppText(text: subTitle[index], size: 30,),
+                      SizedBox(height: 20,),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text: "Mountain hikes give you an incredible sense of freedom along with endurance test",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      ),
+                      SizedBox(height: 40,),
+                      ResponsiveButton()
+                    ],
+                  )
+                ],
+              ),
             ),
           );
         }
---
+++ flutter_cubit_app/lib/widgets/responsive_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+  ResponsiveButton({Key? key, this.width, this.isResponsive=false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor
+      ),
+      child: Row(
+        children: [
+          Image.asset("img/button-one.png"),
+        ],
+      ),
+    );
+  }
+}
