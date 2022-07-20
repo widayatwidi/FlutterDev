ed80b875574ee1bb9eca0340618de9866e8f403b
Widi Widayat <widayatwidi26@gmail.com>
2022-07-20 14:08:23 +0700

    add new step by step dev Flutter app documentation

--- Build Flutter Documentation/app_scrollBgImage/lib/pages/welcome_page.dart
+++ Build Flutter Documentation/app_scrollBgImage/lib/pages/welcome_page.dart
@@ -10,19 +10,28 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView.builder(
         scrollDirection: Axis.vertical,
-        itemCount: 3,
+        itemCount: images.length,
         itemBuilder: (_, index) {
           return Container(
             width: double.maxFinite,
             height: double.maxFinite,
             decoration: BoxDecoration(
               image: DecorationImage(
-                
+                image: AssetImage(
+                  "img/"+images[index]
+                ),
+                fit: BoxFit.cover
               )
             ),
           );
