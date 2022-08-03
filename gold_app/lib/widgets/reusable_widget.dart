9293fe0c1b7c3327d0ad76a0675e85f77b18772d
Widi Widayat <widayatwidi26@gmail.com>
2022-08-03 08:16:03 +0700

    update login & register page

---
+++ gold_app/lib/widgets/reusable_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+hexStringToColor(String hexColor) {
+  hexColor = hexColor.toLowerCase().replaceAll("#", "");
+  if(hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+
+  return Color(int.parse(hexColor, radix: 16));
+}
+
+Image logoWidget(String imageName) {
+  return Image.asset(
+    imageName,
+    fit: BoxFit.fitWidth,
+    width: 100,
+    height: 120,
+  );
+}
