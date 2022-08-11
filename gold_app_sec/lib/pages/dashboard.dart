cbad6f4ed96e68b809134e488aff7f0047f26261
Widi Widayat <widayatwidi26@gmail.com>
2022-08-11 13:08:09 +0700

    update add all widget & add page dashboard

---
+++ gold_app_sec/lib/pages/dashboard.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gold_app_sec/widgets/app_large_text.dart';
+import 'package:gold_app_sec/widgets/app_text.dart';
+import 'package:gold_app_sec/widgets/reusable_widget.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(gradient: LinearGradient(
+          colors: [
+            hexStringToColor("E1D2CE"),
+            hexStringToColor("E1D2CE"),
+            // hexStringToColor("100720")
+          ],
+          begin: Alignment.topCenter, end: Alignment.bottomCenter
+        )),
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            Positioned(
+              top: 40,
+              child: logoWidgetSmall("assets/images/logo.png"),
+            ),
+            Positioned(
+              top: 140,
+              left: 83,
+              right: 83,
+              child: Text("Selamat Datang, Revi!", style: TextStyle(fontSize: 27),textAlign: TextAlign.center,),
+            ),
+            Positioned(
+              top: 220,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: 150,
+                decoration: BoxDecoration(
+                  // color: Color.fromARGB(31, 254, 153, 117),
+                  gradient: LinearGradient(
+                    colors: [
+                      hexStringToColor("FE9975"),
+                      hexStringToColor("FE9975"),
+                      hexStringToColor("FE9975")
+                    ]
+                  ),
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, top: 20),
+                      child: AppText(text: "Emas saya", color: Color.fromARGB(255, 218, 214, 214), size: 14,),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, top: 10),
+                      child: AppLargeText(text: "24,00 gr", color: Color.fromARGB(255, 247, 240, 240), size: 14,),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25, top: 10),
+                      child: AppLargeText(text: "Rp 14.000.000,-", color: Color.fromARGB(255, 245, 242, 242), size: 18,),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 340,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AppText(text: "Keuntungan", size: 13,),
+                      AppLargeText(text: "Rp 2.345.000,-", size: 13, color: Color.fromARGB(137, 123, 36, 36),)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 450,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      hexStringToColor("F8F2EF"),
+                      hexStringToColor("F8F2EF"),
+                      hexStringToColor("F8F2EF")
+                    ]
+                  ),
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Divider(
+                        indent: 120,
+                        endIndent: 120,
+                        color: Color.fromARGB(31, 0, 0, 0),
+                        thickness: 2,
+                      ),
+                      SizedBox(height: 20,),
+                      AppText(text: "Notifikasi Terkini", size: 14,),
+                      SizedBox(height: 20,),
+                      AppLargeText(text: "Penjualan Emas #12344", size: 12,),
+                      SizedBox(height: 10,),
+                      AppText(text: "17 Januari 2022", size: 10,),
+                      SizedBox(height: 10,),
+                      Divider(
+                        color: Color.fromARGB(31, 55, 54, 54),
+                        thickness: 1,
+                      ),
+                      SizedBox(height: 10,),
+                      AppLargeText(text: "Beli Rumah Impian", size: 12,),
+                      SizedBox(height: 10,),
+                      AppText(text: "20 Januari 2022", size: 10,),
+                      SizedBox(height: 10,),
+                      Divider(
+                        color: Color.fromARGB(31, 55, 54, 54),
+                        thickness: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ gold_app_sec/lib/pages/login.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:gold_app_sec/pages/dashboard.dart';
+import 'package:gold_app_sec/pages/register.dart';
+import 'package:gold_app_sec/widgets/app_large_text.dart';
+import 'package:gold_app_sec/widgets/reusable_widget.dart';
+import 'package:gold_app_sec/widgets/text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(gradient: LinearGradient(
+          colors: [
+            hexStringToColor("000000"),
+            hexStringToColor("100F0F"),
+            hexStringToColor("100720")
+          ],
+          begin: Alignment.topCenter, end: Alignment.bottomCenter
+        )),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).size.height * 0.1, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo.png"),
+                SizedBox(height: 50,),
+                Center(
+                  child: AppLargeText(
+                    text: "Selamat Datang di Baby Gold!",
+                    size: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 12,),
+                Center(
+                  child: AppLargeText(
+                    text: "Silahkan lakukan login untuk melanjutkan",
+                    size: 12,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 50,),
+                TextFieldWidget(
+                  hintText: 'Email',
+                  obscureText: false,
+                  prefixIconData: Icons.mail_outline,
+                ),
+                SizedBox(height: 15,),
+                TextFieldWidget(
+                  hintText: 'Password',
+                  obscureText: true,
+                  prefixIconData: Icons.lock_outline,
+                ),
+                SizedBox(height: 30,),
+                Container(
+                  height: 45,
+                  width: MediaQuery.of(context).size.width * 1.2,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromARGB(255, 76, 242, 42),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(9)
+                      )
+                    ),
+                    onPressed: (() {
+                      print("Proses Login");
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder: (context) => DashboardPage())
+                      );
+                    }), 
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 14, color: Colors.black),
+                    )
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Center(
+                  child: TextButton(
+                    child: const Text(
+                      "Lupa kata sandi?",
+                      style: TextStyle(color: Color.fromARGB(255, 76, 242, 42), fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context, 
+                      //   MaterialPageRoute(builder: (context) => LoginPage())
+                      // );
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 43, 42, 41),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Tidak memiliki akun?",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                      TextButton(
+                        child: const Text(
+                          "Daftar di sini.",
+                          style: TextStyle(color: Color.fromARGB(255, 76, 242, 42), fontWeight: FontWeight.w400, fontSize: 12),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(builder: (context) => RegisterPage())
+                          );
+                        },
+                      )
+                    ],
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
